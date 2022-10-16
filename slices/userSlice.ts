c30a0c2d2be5904729987036ef907a4aef0647cb
Aditya Painuli <adityapainuli2004@gmail.com>
2022-10-16 22:57:33 +0530

    refactor code

--- slices/userSlice.ts
+++ slices/userSlice.ts
@@ -1,11 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-export interface UserSlice {
-  user: [];
-}
-
-const initialState: UserSlice = {
+const initialState = {
   user: [],
 };
 
