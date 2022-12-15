92c1172f69634ce80b008744cc05594dc373c46f
Aditya Painuli <adityapainuli2004@gmail.com>
2022-12-15 22:06:22 +0530

    refactor bug

--- outlook-integration/authConfig.js
+++ outlook-integration/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     clientId: "66279fbb-b96c-4c5b-8bc1-40015b0fe7a6",
     authority:
       "https://login.microsoftonline.com/3dec2c66-305c-48fc-bace-e9d62b3bef8e",
-    redirectUri: "https://www.focuspro.app",
+    redirectUri: "https://www.focuspro.app/",
   },
   cache: {
     cacheLocation: "sessionStorage",
