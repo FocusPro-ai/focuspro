0d92923f32f4ba06a47b7ebc6dd8f5f5de7ac4e2
Aditya Painuli <adityapainuli2004@gmail.com>
2022-12-15 22:16:54 +0530

    refactor-code

--- outlook-integration/authConfig.js
+++ outlook-integration/authConfig.js
@@ -3,11 +3,36 @@ export const msalConfig = {
     clientId: "66279fbb-b96c-4c5b-8bc1-40015b0fe7a6",
     authority:
       "https://login.microsoftonline.com/3dec2c66-305c-48fc-bace-e9d62b3bef8e",
-    redirectUri: "https://www.focuspro.app/",
+    redirectUri: "https://www.focuspro.app",
   },
   cache: {
     cacheLocation: "sessionStorage",
     storeAuthStateInCookie: false,
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          default:
+            return;
+        }
+      },
+    },
   },
 };
 
