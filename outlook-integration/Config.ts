9178e9f727c252c1dddbaf6672546f91cdbacc74
Aditya Painuli <adityapainuli2004@gmail.com>
2022-12-17 14:50:10 +0530

    config changes

--- outlook-integration/Config.ts
+++ outlook-integration/Config.ts
@@ -1,6 +1,9 @@
 const config = {
   appId: "66279fbb-b96c-4c5b-8bc1-40015b0fe7a6",
-  redirectUri: "http://localhost:3000",
+  redirectUri:
+    process.env.NODE_ENV === "development" || process.env.APP_STAGING === "true"
+      ? "http://localhost:3000"
+      : "https://www.focuspro.app",
   scopes: ["user.read", "mailboxsettings.read", "calendars.readwrite"],
 };
 
